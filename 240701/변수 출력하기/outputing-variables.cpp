e662d978adf629db42c3a6c58996f59cef925ce5
seungahh <163834460+honghong425@users.noreply.github.com>
2024-07-01 12:09:28 +0900

    Title: 변수 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240701/변수 출력하기/outputing-variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a =3;
+    string b = "C";
+    cout << a;
+    cout << "...";
+    cout << b;
+    return 0;
+}
